22d9544a34a11ab7b0444d587e9cd115c6ffbf82
cyril <jacobcyril042@gmail.com>
2023-10-22 17:17:01 +0200

    fichier conf fait. Manque le bonus,l'interface du serveur, la section critique sur le bean et blindage

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,11 @@
-import Bean.BeanBDgenerique;
-import Bean.BeanBDmetier;
-import Classe.Facture;
 import OVESP.*;
-import ServeurGeneriqueTCP.Logger;
 import ServeurGeneriqueTCP.Protocole;
 import ServeurGeneriqueTCP.ThreadServeur;
 import ServeurGeneriqueTCP.ThreadServeurPool;
 
+import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.List;
+import java.util.Properties;
 
 public class Main {
     public static void main(String[] args) {
@@ -22,17 +13,38 @@ public class Main {
         java.awt.EventQueue.invokeLater(new Runnable() {
 
             public void run() {
+                Properties properties = new Properties();
+                FileInputStream input = null;
+                int port = 0;
+                int nbthread = 0;
+
+                try {
+                    input = new FileInputStream("src\\config.properties");
+                    properties.load(input);
+                    port = Integer.parseInt(properties.getProperty("PORT_PAIEMENT"));
+                    nbthread = Integer.parseInt(properties.getProperty("NB_THREAD"));
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                } finally {
+                    if (input != null) {
+                        try {
+                            input.close();
+                        } catch (IOException e) {
+                            System.out.println("erreur de lecture dans le fichier de conf");
+                        }
+                    }
+                }
                 Protocole protocole;
                 ThreadServeur threadServeur;
                 protocole = new OVESP();
                 try {
-                    //TODO faut faire un fichier de conf pour le numero de port
-                    threadServeur = new ThreadServeurPool(51000, protocole, 3);
+                    //TODO faut faire qqch pour les acces concurrent (mutex,...)
+                    System.out.println("port : "+port+" nbthread = "+nbthread);
+                    threadServeur = new ThreadServeurPool(port, protocole, nbthread);
                 } catch (IOException e) {
                     throw new RuntimeException(e);
                 }
                 threadServeur.start();
-
             }
         });
     }
